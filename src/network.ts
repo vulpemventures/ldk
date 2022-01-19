5696607e86a847ff79e5f41062d9b34c8990233c
João Bordalo <joao.bordalo@gmail.com>
2022-01-19 10:27:12 +0000

    Use cross-fetch instead of isomorphic-fetch (has better support) Minor code improvements

--- src/network.ts
+++ src/network.ts
@@ -1,7 +1,7 @@
-require('isomorphic-fetch');
+import fetch from 'cross-fetch';
 
 /**
- * what to do if fetch() generated an error
+ * throw stringified fetch() error
  * @param response returned from the fetch() call
  */
 const dealWithFetchError = (response: Response) => {
@@ -11,24 +11,21 @@ const dealWithFetchError = (response: Response) => {
 /**
  * check the response type and return it
  * @param response returned from the fetch() call
+ * @returns promise of data
  */
-const getDataFromResponse = async (response: Response) => {
-  const isJson = response.headers
-    .get('content-type')
-    ?.includes('application/json');
+const getDataFromResponse = async (response: Response): Promise<any> => {
+  const contentTypes = response.headers.get('content-type');
+  const isJson = contentTypes?.includes('application/json');
   return isJson ? await response.json() : await response.text();
 };
 
 /**
  * get data from a given url using fetch()
  * @param url url for the webservice
+ * @returns promise of data
  */
-export const getWithFetch = async (url: string) => {
-  const response = await fetch(url, {
-    method: 'GET',
-    mode: 'cors',
-  });
+export const getWithFetch = async (url: string): Promise<any> => {
+  const response = await fetch(url, { method: 'get', mode: 'cors' });
   if (response.status !== 200) dealWithFetchError(response);
-  const data = await getDataFromResponse(response);
-  return data;
+  return await getDataFromResponse(response);
 };
